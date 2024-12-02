c2547b135c4a739e997c4e2c5b7feb15154e645b
Vitória Rabelo <104806771+Vitoria-Rabelo@users.noreply.github.com>
2024-12-02 13:30:07 -0300

    Concluida  atividade Camisa

--- poo/database/camisa/draft.cpp
+++ poo/database/camisa/draft.cpp
@@ -1,6 +1,47 @@
 #include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <array>
+#include <set>
+#include <map>
+#include <vector>
+#include <list>
+#include <unordered_map>
+#include <unordered_set>
+#include <memory>
 
+class Camisa{
+    private:
+    std::string tamanho;
+
+    public:
+    Camisa(std::string tamanho = "") : tamanho(tamanho){}
+
+    std::string getTamanho() const{
+        return tamanho;
+    }
+
+    void setTamanho(std::string valor){
+        if(valor == "PP" || valor == "P" || valor == "M" || valor == "G" || valor == "GG" || valor == "XG"){
+            tamanho = valor;
+        }else{
+            std::cout << "Tamanho informado é inválido.\n";
+        }
+    }
+
+};
 int main() {
-    std::cout << "qxcode\n";
+    Camisa camisa;
+     do {
+        std::cout << "Digite o tamanho da camisa [PP, P, M, G, GG, XG]: ";
+        std::string tamanho;
+        std::cin >> tamanho;
+        camisa.setTamanho(tamanho);
+    } while (camisa.getTamanho().empty());
+
+    std::cout << "O tamanho da camisa é " << camisa.getTamanho() << '\n';
+
+
+    return 0;
 }
 
