c8fa8ee3ad210dff00577999a5bbb131a54ce844
Vitória Rabelo <104806771+Vitoria-Rabelo@users.noreply.github.com>
2024-12-16 23:46:41 -0300

    Implementando Classe Bateria e Int Main

--- poo/database/charger/shell.cpp
+++ poo/database/charger/shell.cpp
@@ -4,7 +4,6 @@
 #include <utility>
 using namespace std;
 
-
 class Carregador{
     private:
         int potencia{0};
@@ -27,7 +26,11 @@ class Bateria{
         int capacidade{0};
         int carga{0};
     public: 
-        Bateria(int capacidade = 0, int carga = 0) : capacidade(capacidade), carga(carga){}
+        Bateria(int capacidade = 0, int carga = 0) : capacidade(capacidade){
+            if (carga == 0) {
+                carga = capacidade;
+            }
+            this->carga = carga;}
 
         int getCapacidade() const{
             return this->capacidade;
@@ -39,11 +42,15 @@ class Bateria{
 
         void setCarga(int newCarga){
             this->carga = newCarga;
+            if(this->carga > this->capacidade){
+                this->carga = this->capacidade;
+            }
         }
+
 
         std::string str() const{
             std::stringstream ss;
-            ss << getCarga() << ":" << getCapacidade() << "\n";
+            ss << this->carga << "/" << this->capacidade;
             return ss.str();
         }
 };
@@ -62,8 +69,41 @@ class Notebook{
             this->tempo = 0;
         }
 
-        void setCarregador(std::shared_ptr<Carregador> newCarregador){
-            this->carregador = newCarregador;
+        void setCarregador(std::shared_ptr<Carregador> novoCarregador){
+            if(this->carregador == nullptr){
+                this->carregador = novoCarregador;
+                }else{
+                    std::cout << "fail: carregador já conectado\n";
+                }
+        }
+
+        void removeCarregador(){
+            if(this->carregador != nullptr){
+                std::cout << "Removido " << this->carregador->getPotencia() << "W" <<std::endl;
+                this->carregador = nullptr;
+                this->ligado = false;
+            }else{
+                std::cout << "fail: Sem carregador\n";
+            }
+        }
+
+        void setBateria(std::shared_ptr<Bateria> novaBateria){
+            if(this->bateria == nullptr){
+            this->bateria = novaBateria;
+            }else{
+                std::cout << "fail: já existe bateria\n";
+            }
+        }
+
+        void removeBateria(){
+            if(this->bateria != nullptr){
+                std::cout << "Removido " << this->bateria->getCarga() << "/" << this->bateria->getCapacidade() <<
+                std::endl;
+                this->bateria = nullptr;
+                this->ligado = false;
+            }else{
+                std::cout << "fail: Sem bateria\n";
+            }
         }
 
         void ligar(){
@@ -85,10 +125,20 @@ class Notebook{
                 std::cout << "fail: desligado\n";
             }
             if(this->bateria != nullptr && this->carregador == nullptr){
-                this->bateria->setCarga(this->bateria->getCarga() - tempo);
+                if(this->bateria->getCarga() <= tempo){
+                    std::cout << "fail: descarregou\n";
+                    this->bateria->setCarga(0);
+                    this->ligado = false;
+                }else{
+                    this->bateria->setCarga(this->bateria->getCarga() - tempo);
+                    if(this->bateria->getCarga() < 0){
+                        this->bateria->setCarga(0);
+                    }
+                }  
             }
             if(this->bateria != nullptr && this->carregador != nullptr){
                 this->bateria->setCarga(this->bateria->getCarga() + tempo);
+                
             }
         }
 
@@ -97,24 +147,24 @@ class Notebook{
             std::cout << "Notebook: desligado\n";
             }
             if(ligado == false && this->bateria != nullptr && this->carregador != nullptr){
-            std::cout << "Notebook: desligado,Bateria: " << this->bateria << ", " << 
+            std::cout << "Notebook: desligado,Bateria " << this->bateria << ", " << 
             this->carregador->str() << std::endl;
             }
             if(ligado == false && this->bateria != nullptr && this->carregador == nullptr){
-            std::cout << "Notebook: desligado, Bateria:" << this->bateria->str() << std::endl;
+            std::cout << "Notebook: desligado, Bateria " << this->bateria->str() << std::endl;
             }
             if(ligado == false && this->bateria == nullptr && this->carregador != nullptr){
             std::cout << "Notebook: desligado, " << this->carregador->str() << std::endl;
             }
             if(ligado == true && this->bateria == nullptr && this->carregador == nullptr){
-            std::cout << "Notebook: ligado";
+            std::cout << "Notebook: ligado\n";
             }
             if(ligado == true && this->bateria != nullptr && this->carregador != nullptr){
-            std::cout << "Notebook: ligado por " << this->tempo << " min, Bateria: " << this->bateria << ", " << 
-            this->carregador->str() << std::endl;
+            std::cout << "Notebook: ligado por " << this->tempo << " min, Carregador "<< this->carregador->getPotencia() 
+            << "W, Bateria " << this->bateria->str() <<std::endl;
             }
             if(ligado == true && this->bateria != nullptr && this->carregador == nullptr){
-            std::cout << "Notebook: ligado por " << this->tempo << " min, Bateria: " << this->bateria->str() << std::endl;
+            std::cout << "Notebook: ligado por " << this->tempo << " min, Bateria " << this->bateria->str() << std::endl;
             }
             if(ligado == true && this->bateria == nullptr && this->carregador != nullptr){
             std::cout << "Notebook: ligado por " << this->tempo << " min, " << this->carregador->str() << std::endl;
@@ -146,13 +196,14 @@ int main() {
             auto carregador = std::make_shared<Carregador>(potencia);
             note.setCarregador(carregador);
         } else if (cmd == "rm_charger") {
-            // REMOVA O CARREGADOR DO NOTEBOOK E IMPRIMA SE ELE EXISTIR
+            note.removeCarregador();
         } else if (cmd == "set_battery") {
-            // CRIE UM OBJETO Bateria E ATRIBUA AO NOTEBOOK
-            // int capacity;
-            // ss >> capacity;
+            int capacidade;
+            ss >> capacidade;
+            auto bateria = std::make_shared<Bateria>(capacidade);
+            note.setBateria(bateria);
         } else if (cmd == "rm_battery") {
-            // REMOVA A BATERIA DO NOTEBOOK E IMPRIMA SE ELA EXISTIR
+            note.removeBateria();
         } else if (cmd == "end") {
             break;
         } else {
