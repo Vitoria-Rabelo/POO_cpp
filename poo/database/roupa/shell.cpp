74a0567eb8549ff447cc2ee4c634b276c6c09d4f
Vitória Rabelo <104806771+Vitoria-Rabelo@users.noreply.github.com>
2024-12-17 10:24:57 -0300

    Concluida  Atividade Roupa com testes

--- poo/database/roupa/shell.cpp
+++ poo/database/roupa/shell.cpp
@@ -1,26 +1,53 @@
-#include <iostream>
-#include <vector>
-#include <sstream>
-using namespace std;
+#include "fn.hpp"
+
+class Towel{
+    private:
+        std::string size;
+    public:
+        Towel(){
+            this->size = "";
+        }
+
+        std::string getSize() const{
+            return this->size;
+        }
+
+        void setSize(std::string newSize){
+            if(newSize == "PP" || newSize == "P" || newSize == "M" || newSize == "GG" ||
+                newSize == "XG"){
+                    this->size = newSize;
+            }else{
+                std::cout << "fail: Valor inválido, tente PP, P, M, G, GG ou XG\n";
+            }
+
+        }
+};
+
+class Adapter {
+    std::shared_ptr<Towel> towel;
+public:
+    Adapter() {
+        this->towel = std::make_shared<Towel>();
+    }
+    void size(const std::string& size) {
+        this->towel->setSize(size);
+    }
+    void show() {
+        std::cout << "size: (" << this->towel->getSize() << ")\n";
+    }
+};
 
 int main() {
-    // CRIE SEU OBJETO AQUI
+    Adapter adapter;
     while (true) {
-        string line, cmd;
-        getline(cin, line);
-        cout << "$" << line << endl;
+        fn::write("$", "");
+        auto line = fn::input();
+        auto args = fn::split(line, ' ');
+        fn::write(line);
 
-        stringstream ss(line);
-        ss >> cmd;
-
-        if (cmd == "size") { // TENTE ATRIBUIR UM TAMANHO
-            // string size;
-            // ss >> size;
-        } else if (cmd == "show") { // MOSTRE AS INFORMAÇÕES DO OBJETO
-        } else if (cmd == "end") {
-            break;
-        } else {
-            cout << "fail: Comando inválido" << endl;
-        }
+        if      (args[0] == "end")  { break;                         }
+        else if (args[0] == "size") { adapter.size(args[1]);         }
+        else if (args[0] == "show") { adapter.show();                }
+        else                        { fn::write("Comando inválido"); }
     }
 }
