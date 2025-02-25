65d3824e14c09e1441de7b746154e2bff3fb3e5c
Vitória Rabelo <104806771+Vitoria-Rabelo@users.noreply.github.com>
2025-02-25 00:26:39 -0300

    implementando metodo remove

--- poo/database/pula-pula/shell.cpp
+++ poo/database/pula-pula/shell.cpp
@@ -45,21 +45,45 @@ public:
     Trampoline() {}
 
     void arrive(Kid kid) {       
-        waiting.push_front(kid);
+        waiting.push_front(kid);    
     }
 
     void enter() {
         if (!waiting.empty()) {
-            playing.push_back(waiting.back());
+            playing.push_front(waiting.back()); 
             waiting.pop_back();
         }
     }
-    
 
     void leave() {
         if (!playing.empty()) {
-            playing.pop_front();
+            Kid kid = playing.back(); 
+            waiting.push_front(kid); 
+            playing.pop_back(); 
         }
+    }
+
+    void remove(string name){
+        auto it_waiting = waiting.begin();
+        while (it_waiting != waiting.end()) {
+            if (it_waiting->getName() == name) {
+                waiting.erase(it_waiting); 
+                return;
+            }
+            it_waiting++;
+        }
+
+        auto it_playing = playing.begin();
+        while (it_playing != playing.end()) {
+            if (it_playing->getName() == name) {
+                playing.erase(it_playing); 
+                return;
+            }
+            it_playing++;
+        }
+        
+        cout << "fail: " << name << " nao esta no pula-pula" << endl;
+
     }
 
     void show() {
@@ -96,7 +120,11 @@ int main() {
             trampoline.enter();
         } else if (cmd == "leave") {
             trampoline.leave();
-        } else {
+        } else if(cmd == "remove"){
+            string name;
+            ss >> name;
+            trampoline.remove(name);
+        }else {
             cout << "fail: comando invalido" << endl;
         }
     }
