7863d59d95e8a4d5c462cf9947fd03f237a21c46
Vitória Rabelo <104806771+Vitoria-Rabelo@users.noreply.github.com>
2025-03-07 21:40:58 -0300

    Criando classe veiculos e implementando polimorfismo

--- poo/database/tarifas/shell.cpp
+++ poo/database/tarifas/shell.cpp
@@ -1,205 +1,77 @@
 #include <iostream>
+#include <string>
 #include <vector>
-#include <sstream>
+#include <algorithm>
 #include <iomanip>
-#include <list>
+#include <sstream>
 using namespace std;
 
-template<typename CONTAINER, typename FUNC>
-string join(const CONTAINER& cont, FUNC func, const string& delim) {
-    stringstream ss;
-    for (auto it = cont.begin(); it != cont.end(); it++) {
-        ss << (it == cont.begin() ? "" : delim) << func(*it);
-    }
-    return ss.str();
-}
+class Veiculo{
+    string id;
+    string tipo;
+    int horaEntrada;
 
-enum Label {
-    DEPOSIT,
-    FEE,
-    OPENING,
-    REVERSE,
-    WITHDRAW,
-    ERROR
+    public:
+        Veiculo(string id = " ", string tipo = " ", int horaEntrada = 0): id(id), tipo(tipo), horaEntrada(horaEntrada){};
+        virtual ~Veiculo() {};
+
+         string getId(){
+            return id;
+         }
+
+         string getTipo(){
+            return tipo;
+         }
+
+         int getHoraEntrada(){
+            return horaEntrada;
+         }
+
+         void setHoraEntrada(int newHora){
+            this->horaEntrada = newHora;
+         }
+
+         virtual double calcularValor(int horaSair) = 0;
+
+
+         virtual string str() const{
+            stringstream ss;
+            ss << "______" << this->tipo << " : " << setfill('-') << setw(8) << this->id << " : " << this->horaEntrada;
+            return ss.str();
+         }
+
 };
 
-class Operation {
-    int index;
-    Label label;
-    int value;
-    int balance;
-
-public:
-    Operation(int index, Label label, int value, int balance) 
-        : index(index), label(label), value(value), balance(balance) {};
-
-    int getIndex() const {
-        return index;
-    }
-
-    Label getLabel() const {
-        return label;
-    }
-
-    int getValue() const {
-        return value;
-    }
-
-    int getBalance() const {
-        return balance;
-    }
-
-    string str() const {
-        stringstream ss;
-        ss << setw(2) << index << ": ";
-        switch (label) {
-            case DEPOSIT:
-                ss << " deposit:" << setw(5) << value;
-                break;
-            case FEE: 
-                if(value > 9) {
-                    ss << "     fee:" << setw(3) << "-" << value;
-                } else
-                ss << "     fee:" << setw(4) << "-" << value;
-                break;
-            case OPENING: 
-                ss << " opening:" << setw(5) << value;
-                break;
-            case REVERSE: 
-                ss << " reverse:" << setw(5) << value; 
-                break;
-            case WITHDRAW: 
-                ss << "withdraw:" << setw(3) << "-" << value ; 
-                break;
-            case ERROR: 
-                ss << "   error:" << setw(5) << value; 
-                break;
-        }
-
-
-        ss << ": " << setw(4) << balance;
-        return ss.str();
-    }
+class Bike : public Veiculo{
+    public:
+       Bike(string id = " ", int horaEntrada = 0) : Veiculo(id, "Bike", horaEntrada){};
+       double calcularValor(){
+           return 3.00;
+       }
 };
 
-class BalanceManager {
-    int balance;
-    int nextId;
-    list<Operation> extract;
-
-public:
-    BalanceManager(int balance = 0) : balance(balance), nextId(0) {
-        addOperation(OPENING, balance);
-    }
-
-    int getBalance() const {
-        return balance;
-    }
-
-    void addOperation(Label label, int value) {
-        if (label == DEPOSIT) {
-            balance += value;
-        } else if (label == WITHDRAW || label == FEE) {
-            balance -= value;
-        } else if(label == REVERSE){
-            balance += value;
-        }
-        Operation op(nextId++, label, value, balance);
-        extract.push_back(op);
-    }
-
-    void extractBalance(int qtd) const {
-        int count = 0;
-        if(qtd == 0){
-            for (const auto& op : extract) {
-                if (count >= qtd && qtd != 0) break;
-                cout << op.str() << endl; 
-                count++;
-            }
-        }
-        else{
-            vector<Operation> lastOps;
-        size_t limit = static_cast<size_t>(qtd); 
-
-        for (auto rit = extract.rbegin(); rit != extract.rend() && lastOps.size() < limit; ++rit) {
-            lastOps.push_back(*rit);
-        }
-        for (auto it = lastOps.rbegin(); it != lastOps.rend(); ++it) {
-            cout << it->str() << endl;
-        }
-        }
-        
-    }
-
-    const list<Operation>& getExtract() const {
-        return extract;
-    }
-
-    void show() const {
-        cout << "balance:" << balance << endl;
-    }
+class Moto : public Veiculo{
+    public:
+       Moto(string id = " ", int horaEntrada = 0) : Veiculo(id, "Moto", horaEntrada){};
+       double calcularValor(int horaSair){
+              return (horaSair - getHoraEntrada()) / 20.0;
+       }
 };
 
-class Account {
-    int id;
-    BalanceManager balanceManager;
-
-public:
-    Account(int id = 0, int initBalance = 0) : id(id), balanceManager(initBalance) {}
-
-    void deposit(int value) {
-        if (value < 0) {
-            cout << "fail: invalid value" << endl;
-            return;
-        }
-        balanceManager.addOperation(DEPOSIT, value);
-    }
-
-    void withdraw(int value) {
-        if (value > balanceManager.getBalance()) {
-            cout << "fail: insufficient balance" << endl;
-            return;
-        }
-        balanceManager.addOperation(WITHDRAW, value);
-    }
-
-    void fee(int value) {
-        balanceManager.addOperation(FEE, value);
-    }
-
-    void extract(int qtd) {
-        balanceManager.extractBalance(qtd);
-    }
-
-    void reverse(const vector<int>& values) {
-        const list<Operation>& operations = balanceManager.getExtract();
-    
-        for (int value : values) {
-            auto it = operations.end();
-            while (it != operations.begin()) {
-                --it; 
-                if (it->getIndex() == value) {
-                    if (it->getLabel() != FEE) {
-                        cout << "fail: index " << value << " is not a fee" << endl;
-                    } else {
-                        balanceManager.addOperation(REVERSE, it->getValue());
+class Carro : public Veiculo{
+    public:
+       Carro(string id = " ", int horaEntrada = 0) : Veiculo(id, "Carro", horaEntrada){};
+         double calcularValor(int horaSair){
+                    double valorPagar = (horaSair - getHoraEntrada()) / 10.0;
+                    if(valorPagar < 5.0){
+                        return 5.0;
                     }
-                    goto next_value;
-                }
-            }
-            cout << "fail: index " << value << " invalid" << endl;
-        next_value:;
-        }
-    }
-
-    void show() const {
-        cout << "account:" << id << " ";
-        balanceManager.show();
-    }
+                    return valorPagar;
+         }
 };
 
 int main() {
-    Account account(0);
+    
     while (true) {
         string line, cmd;
         getline(cin, line);
@@ -210,38 +82,18 @@ int main() {
 
         if (cmd == "end") {
             break;
-        } else if (cmd == "init") {
-            int number;
-            ss >> number;
-            account = Account(number);
         } else if (cmd == "show") {
-            account.show();
-        } else if (cmd == "deposit") {
-            int value;
-            ss >> value;
-            account.deposit(value);
-        } else if (cmd == "withdraw") {
-            int value;
-            ss >> value;
-            account.withdraw(value);
-        } else if (cmd == "fee") {
-            int value;
-            ss >> value;
-            account.fee(value);
-        } else if (cmd == "extract") {
-            int qtd;
-            ss >> qtd;
-            account.extract(qtd);
-        } else if(cmd == "reverse"){
-            vector<int> values;
-            int value;
-            while (ss >> value) { 
-                values.push_back(value);
-            }
-            account.reverse(values);
-        }else {
-            cout << "fail: invalid command\n";
+        } else if (cmd == "estacionar") {
+            // string tipo, id;
+            // ss >> tipo >> id;
+        } else if (cmd == "tempo") {
+            // int tempo {};
+            // ss >> tempo;
+        } else if (cmd == "pagar") {
+            // string id;
+            // ss >> id;
+        } else {
+            cout << "fail: comando invalido" << endl;
         }
     }
-    return 0;
 }
